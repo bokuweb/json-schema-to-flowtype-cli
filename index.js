d1de6284448d4367c38903805c9930e9c4628d61
bokuweb <bokuweb12@gmail.com>
2017-04-24 14:33:19 +0900

    Add params

--- index.js
+++ index.js
@@ -6,33 +6,37 @@ const yaml = require('js-yaml');
 const mkdirp = require('mkdirp');
 const { parseSchema } = require('json-schema-to-flow-type');
 
-const SCHEMA_FILES = './samples/**/*.+(json|yml|yaml)';
-const OUT = './__gen__';
+const SCHEMA_FILES = '/**/*.+(json|yml|yaml)';
 
-const spinner = new Spinner('processing... %s');
-spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏');
-spinner.start();
+const generate = (params) => {
 
-const files = glob.sync(SCHEMA_FILES);
+  const spinner = new Spinner('processing... %s');
+  spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏');
+  spinner.start();
 
-files.forEach(file => {
-  try {
-    const schema = readFileSync(file, 'utf8')
-    const extname = path.extname(file);
-    const json = extname === '.yml' || extname === '.yaml'
-      ? yaml.safeLoad(schema)
-      : JSON.parse(schema);
-    const dir = path.join(OUT, path.dirname(file));
-    mkdirp.sync(dir);
-    const flow = '/* @flow */\n\n' + parseSchema(json);
-    const filename = path.join(dir, path.basename(file, extname)) + '.js';
-    writeFileSync(filename, flow);
-  } catch (e) {
-    console.error(e);
-    spinner.stop(true);
-    process.exit(1);
-  }
-});
+  const files = glob.sync(path.join(params.targetDir, SCHEMA_FILES));
 
-spinner.stop(true);
+  files.forEach(file => {
+    try {
+      const schema = readFileSync(file, 'utf8')
+      const extname = path.extname(file);
+      const json = extname === '.yml' || extname === '.yaml'
+        ? yaml.safeLoad(schema)
+        : JSON.parse(schema);
+      const dir = path.join(params.outputDir, path.relative(params.targetDir, path.dirname(file)));
+      mkdirp.sync(dir);
+      const flow = '/* @flow */\n\n' + parseSchema(json);
+      const filename = path.join(dir, path.basename(file, extname)) + '.js';
+      writeFileSync(filename, flow);
+    } catch (e) {
+      console.error(e);
+      spinner.stop(true);
+      process.exit(1);
+    }
+  });
+  spinner.stop(true);
+};
 
+generate({ targetDir: './samples', outputDir: '__gen__' });
+
+module.exports = generate;
