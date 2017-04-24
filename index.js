014ae15e0d47ce260ecbcea8970068412ab0f19d
bokuweb <bokuweb12@gmail.com>
2017-04-24 11:10:56 +0900

    Support yaml.

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
+const path = require('path');
 const { Spinner } = require('cli-spinner');
 const glob = require('glob');
+const yaml = require('js-yaml');
 const mkdirp = require('mkdirp');
 const { parseSchema } = require('json-schema-to-flow-type');
-const schema = require('./samples/json-schema-sample01');
-const SCHEMA_FILES = './samples/**/*.+(json|yml|yaml)';
 
-const spinner = new Spinner('[Processing].. %s');
-spinner.setSpinnerString('|/-\\');
+const SCHEMA_FILES = './samples/**/*.+(json|yml|yaml)';
+const OUT = './__gen__';
+
+const spinner = new Spinner('processing... %s');
+spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏');
 spinner.start();
 
 const files = glob.sync(SCHEMA_FILES);
 
-console.log(files)
+files.forEach(file => {
+  try {
+    const schema = readFileSync(file, 'utf8')
+    const extname = path.extname(file);
+    const json = extname === '.yml' || extname === '.yaml'
+      ? yaml.safeLoad(schema)
+      : JSON.parse(schema);
+    const dir = path.join(OUT, path.dirname(file));
+    mkdirp.sync(dir);
+    const flow = '/* @flow */\n\n' + parseSchema(json);
+    const filename = path.join(dir, path.basename(file, extname)) + '.js';
+    writeFileSync(filename, flow);
+  } catch (e) {
+    console.error(e);
+    spinner.stop(true);
+    process.exit(1);
+  }
+});
 
-const flow = parseSchema(schema);
-writeFileSync('./flow.js', flow);
 spinner.stop(true);
+
