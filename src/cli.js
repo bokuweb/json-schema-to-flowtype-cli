ba76e9fa14018731e9df719810a383e1e226e253
bokuweb <bokuweb12@gmail.com>
2017-04-24 14:50:28 +0900

    Add cli

---
+++ src/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/* @flow */
+
+const meow = require('meow');
+const generate = require('./');
+
+if (!process.argv[2]) {
+  log.fail('please specify actual, expected and diff images directory.');
+  log.fail('e.g.: $ reg-cli /path/to/actual-dir /path/to/expected-dir /path/to/diff-dir');
+  process.exit(1);
+}
+
+const cli = meow(`
+  Usage
+    $ reg-cli /path/to/actual-dir /path/to/expected-dir /path/to/diff-dir
+  Options
+    -U, --update Update expected images.(Copy \`actual images\` to \`expected images\`).
+  Examples
+    $ reg-cli /path/to/actual-dir /path/to/expected-dir /path/to/diff-dir -U -D ./reg.json
+`, {
+    alias: {
+      O: 'outputDir',
+    },
+  });
+
+generate({
+  targetDir: process.argv[2],
+  outputDir: cli.flags.outputDir,
+});
